6e3dbd33a5c25ade5582cc2e8e4938268003271c
Ruan Silva <ruan.silva2002@gmail.com>
2026-06-21 23:59:59 -0300

    feat: adiciona novas funcionalidades no UsuarioService

--- src/main/java/com/ruan/workout/service/UsuarioService.java
+++ src/main/java/com/ruan/workout/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.ruan.workout.service;
 
 import com.ruan.workout.dto.LoginDTO;
 import com.ruan.workout.dto.UsuarioDTO;
+import com.ruan.workout.exception.LoginException;
 import com.ruan.workout.exception.ValidationUsuarioException;
 import com.ruan.workout.model.Usuario;
 import com.ruan.workout.repository.UsuarioRepository;
@@ -14,16 +15,37 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
     
     public void cadastro(UsuarioDTO dto) {
-        if (usuarioRepository.existsUsuarioByEmailOrUsuario(dto.email(), dto.usuario())){
-            throw new ValidationUsuarioException("Email ou Usuário já em uso!");
+        if (usuarioRepository.existsUsuarioByEmailOrUsuario(dto.usuario(), dto.email())) {
+            throw new ValidationUsuarioException("Usuario já cadastrado! ");
         }
-        usuarioRepository.save(new Usuario(dto.usuario(), dto.name(), dto.email(), dto.password(), dto.idade()));
-    }
-
-    public String login(LoginDTO dto) {
-        return "";
+        usuarioRepository.save(new Usuario(dto.usuario(), dto.password(), dto.email(), dto.name(), dto.idade()));
     }
 
     public void atualizarInformacoesUsuario(UsuarioDTO dto) {
+        if (!usuarioRepository.existsUsuarioByEmailOrUsuario(dto.usuario(), dto.email())) {
+            throw new ValidationUsuarioException("Usuario não cadastrado!");
+        }
+
+        Usuario usuario = usuarioRepository.findByUsuario(dto.usuario());
+        usuario.setEmail(dto.email());
+        usuario.setPassword(dto.password());
+        usuario.setIdade(dto.idade());
+        usuario.setName(dto.name());
+        usuarioRepository.save(usuario);
     }
+
+    public String login(LoginDTO dto) {
+        Usuario usuario = usuarioRepository.findByUsuario(dto.usuario());
+
+        if (usuario == null) {
+            throw new ValidationUsuarioException("Usuario não cadastrado!");
+        }
+
+        if (usuario.getPassword().equals(dto.password())) {
+            throw new LoginException("Senha Incorreta!");
+        }
+
+        return "Login realizado com sucesso!";
+    }
+
 }
