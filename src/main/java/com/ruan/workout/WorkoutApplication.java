6b954b3cf4d079de73ba15f8311cd57c6a6bc2f3
Ruan Silva <ruan.silva2002@gmail.com>
2026-07-09 07:36:06 -0300

    Coloca novos imports na classe principal

--- src/main/java/com/ruan/workout/WorkoutApplication.java
+++ src/main/java/com/ruan/workout/WorkoutApplication.java
@@ -2,11 +2,15 @@ package com.ruan.workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class WorkoutApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(WorkoutApplication.class, args);
 	}
 
